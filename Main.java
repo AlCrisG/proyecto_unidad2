c435ab7d3280bf881c7acc770733913b8f0c6820
HiramMR <montanorodriguez.hiram.proga@gmail.com>
2024-03-29 15:05:17 -0600

    Se modificó README y se agregó Main y Zoosystem

---
+++ Main.java
@@ -0,0 +1,7 @@
+package proyecto_unidad2;
+public class Main {
+    public static void main(String[] args) {
+        ZooSystem system = new ZooSystem();
+        system.ExecuteProgram();
+    }
+}
---
+++ ZooSystem.java
@@ -0,0 +1,62 @@
+package proyecto_unidad2;
+import java.util.Scanner;
+
+public class ZooSystem {
+    private final String PASSWORD = "ZooManager";
+    private Scanner scanner = new Scanner(System.in);
+
+    public void ExecuteProgram() {
+        boolean isCorrectPassword = false;
+
+        do {
+            System.out.println("\n*** BIENVENIDO ***");
+            System.out.println("Ingresa la contraseña");
+            String password = scanner.nextLine();
+
+            if (password.equals(PASSWORD)) {
+                isCorrectPassword = true;
+                executeMenu();
+            } else {
+                System.out.println("\nContraseña incorrecta, intenta de nuevo");
+            }
+        } while (!isCorrectPassword);
+    }
+
+    private void executeMenu() {
+
+        do {
+            System.out.println("+---------------------+");
+            System.out.println("|        MENU         |");
+            System.out.println("+---------------------+");
+            System.out.println("| OPCION | DESCRIPCION|");
+            System.out.println("+---------------------+");
+            System.out.println("|   1    | Registrar  |");
+            System.out.println("|   2    | Modificar  |");
+            System.out.println("|   3    | Eliminar   |");
+            System.out.println("|   4    | Consultar  |");
+            System.out.println("|   5    | Salir      |");
+            System.out.println("+---------------------+");
+            System.out.print("Elige una opción: ");
+                int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                System.out.println("xd");
+                    break;
+                case 2:
+                System.out.println("xd");
+                    break;
+                case 3:
+                System.out.println("xdx");
+                    break;
+                case 4:
+                System.out.println("xd");
+                    break;
+                case 5:
+                System.out.println("Terminando sesion...");
+                    System.exit(0);
+                    break;
+            }
+        } while(true);
+    }
+}
